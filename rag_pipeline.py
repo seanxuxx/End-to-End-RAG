e1d27e21b7d08ee8c319b9fc1bd2cd9f397781e4
xx-aws <xiaoxu1094@gmail.com>
2025-03-15 03:25:34 +0000

    Update pipeline

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -10,16 +10,16 @@ from langchain_community.document_loaders import DirectoryLoader
 from langchain_core.documents import Document
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_huggingface.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from pinecone import Pinecone, ServerlessSpec
 from pinecone.data.index import Index
 from tqdm import tqdm
 from transformers import (AutoModelForCausalLM, AutoTokenizer,
-                          BitsAndBytesConfig, pipeline)
+                          BitsAndBytesConfig, GenerationConfig,
+                          TextGenerationPipeline, pipeline)
 
-from utils import ParagraphTextSplitter, set_logger
+from utils import get_chunk_max_length, set_logger
 
 load_dotenv()
 pinecone_api_key = os.getenv('PINECONE_API_KEY')
@@ -32,41 +32,17 @@ DEVICE = ('cuda' if torch.cuda.is_available() else
           'mps' if torch.backends.mps.is_available() else 'cpu')
 
 
-PROMPT_IN_CHAT_FORMAT = [
-    {
-        "role": "system",
-        "content": """Using the information contained in the context,
-give a comprehensive answer to the question.
-Respond only to the question asked, response should be concise and relevant to the question.
-If the answer cannot be deduced from the context, do not give an answer.""",
-    },
-    {
-        "role": "user",
-        "content": """Context:
+PROMPT_IN_CHAT_FORMAT = """\
 {context}
----
-Now here is the question you need to answer.
-
-Question: {question}""",
-    },
-]
-
-
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    # DataStore paramters
-    parser.add_argument('--embedding_model', type=str, default='all-mpnet-base-v2')
-    parser.add_argument('--chunker_name', type=str, default='character_chunker')
-    parser.add_argument('--chunk_size', type=int, default=500)
-    parser.add_argument('--chunk_overlap', type=int, default=100)
-    # Retriver parameters
-    parser.add_argument('--llm_model', type=str, default='mistralai/Mistral-7B-Instruct-v0.2')
-    return parser.parse_args()
+----------
+Using the information contained in the context,
+give a concise, accurate answer to the question.
+Question: {question}
+"""
 
 
 class Query(TypedDict):
     """
-    _summary_
     Using TypedDict keeps track of input question, retrieved context, and generated answer,
     helping maintain a structured, type-safe, and modular workflow.
     The idea is adopted from Build a Retrieval Augmented Generation (RAG) App: Part 1 | 🦜️🔗 LangChain
@@ -78,40 +54,29 @@ class Query(TypedDict):
 
 
 class DataStore():
-    def __init__(self, model_name: str,
-                 chunker_name: str, chunk_size=500, chunk_overlap=100,
-                 dir_to_chunk='raw_data', dir_preformatted='',
-                 filename_pattern='**/*.txt', is_upsert_data=False):
+    def __init__(self, model_name: str, data_dir: str,
+                 chunk_size=500, chunk_overlap=100,
+                 filename_pattern='**/*.txt',
+                 is_semantic_chunking=False, is_upsert_data=False):
         """
         Args:
             model_name (str): Name of HuggingFaceEmbeddings model.
-            chunker_name (str): Chunking method.
-                'character_chunker' for RecursiveCharacterTextSplitter;
-                'semantic_chunker' for SemanticChunker;
-                other string will raise an error.
+            data_dir (str): Directory storing raw data files.
             chunk_size (int, optional): Defaults to 500.
             chunk_overlap (int, optional): Defaults to 100.
-            dir_to_chunk (str, optional):
-                Directory storing raw data files. Defaults to 'raw_data'.
-            dir_preformatted (str, optional):
-                Directory storing pre-formatted data files. Defaults to ''.
-            filename_pattern (str, optional):
-                "glob" parameter for DirectoryLoader. Defaults to '**/*.txt'.
+            filename_pattern (str, optional): "glob" parameter for DirectoryLoader. Defaults to '**/*.txt'.
+            is_semantic_chunking (bool, optional):
+                True for using SemanticChunker, otherwise RecursiveCharacterTextSplitter. Defaults to False.
             is_upsert_data (bool, optional):
                 Whether to upsert documents to vector store. Defaults to False.
         """
         # Data config
-        assert os.path.exists(dir_to_chunk), f"{dir_to_chunk} does not exist"
-        if dir_preformatted:
-            assert os.path.exists(dir_preformatted), f"{dir_preformatted} does not exist"
-        self.dir_to_chunk = dir_to_chunk
-        self.dir_preformatted = dir_preformatted
+        self.data_dir = data_dir
         self.filename_pattern = filename_pattern
 
         # Chunking config
-        chunker_options = ['character_chunker', 'semantic_chunker']
-        assert chunker_name in chunker_options, f"{chunker_name} is invalid chunker"
-        self.chunker_name = chunker_name
+        chunker_name = 'semantic' if is_semantic_chunking else 'recursive'
+        self.is_semantic_chunking = is_semantic_chunking
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
 
@@ -130,8 +95,6 @@ class DataStore():
         self.pc_index = self.get_pinecone_index()
         self.vector_store = PineconeVectorStore(index=self.pc_index, embedding=self.embeddings)
         self.retriever = self.vector_store.as_retriever()
-
-        logging.info(f"Initialized Retriver model:\n{self.__dict__}\n")
 
         if self.is_upsert_data:
             self.upsert_vector_store()
@@ -170,75 +133,82 @@ class DataStore():
         Returns:
             list[Document]
         """
-        # Load raw documents and split them into chunks
-        loader = DirectoryLoader(self.dir_to_chunk,
-                                 glob=self.filename_pattern,
-                                 show_progress=True,
-                                 use_multithreading=True)
+        # Load raw documents
+        assert os.path.exists(self.data_dir), f"{self.data_dir} does not exist"
+        loader = DirectoryLoader(self.data_dir, glob=self.filename_pattern,
+                                 show_progress=True, use_multithreading=True)
         docs = loader.load()
-        if self.chunker_name == 'semantic_chunker':
-            text_splitter = SemanticChunker(self.embeddings)
-        else:
+
+        # Chunk documents
+        if self.is_semantic_chunking:  # SemanticChunker + potentail RecursiveCharacterTextSplitter
+            semantic_splitter = SemanticChunker(self.embeddings)
+            semantic_chunks = [chunk for doc in tqdm(docs, desc='Chunk docs')
+                               for chunk in semantic_splitter.split_documents([doc])]
+            # Sub-chunk long documents where the document length falls in the upper outlier range
+            max_length = min(get_chunk_max_length(semantic_chunks), 40000)
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=max_length,
+                                                           chunk_overlap=int(max_length*0.1))
+            chunks = []
+            for chunk in semantic_chunks:
+                if len(chunk.page_content) < max_length:
+                    chunks.append(chunk)
+                else:  # Document length outliers
+                    chunks.extend(text_splitter.split_documents([chunk]))
+        else:  # Pure RecursiveCharacterTextSplitter
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size,
                                                            chunk_overlap=self.chunk_overlap)
-        chunks = [chunk for doc in tqdm(docs, desc='Chunk docs')
-                  for chunk in text_splitter.split_documents([doc])]
-
-        # Load pre-formatted documents and add to chunks
-        if self.dir_preformatted:
-            loader = DirectoryLoader(self.dir_preformatted,
-                                     glob=self.filename_pattern,
-                                     show_progress=True,
-                                     use_multithreading=True)
-            docs = loader.load()
-            text_splitter = ParagraphTextSplitter()
-            chunks.extend(text_splitter.split_documents(docs))
+            chunks = text_splitter.split_documents(docs)
 
         # Add IDs for the documents
         for i, doc in tqdm(enumerate(chunks), total=len(chunks), desc='Add doc id'):
-            id_text = f"{self.chunker_name}_{i}_{doc.metadata['source']}"
+            id_text = f"{i}_{doc.metadata['source']}"
             id_text = re.sub(r'[^\w]', '_', id_text).lower()
             doc.id = id_text
 
         # Index documents
-        logging.info(f"Upserting {len(chunks)} chunks to Index {self.index_name}...")
+        logging.info(f'Upserting {len(chunks)} chunks to Index "{self.index_name}"...')
         self.vector_store.add_documents(chunks)
-        logging.info("Done\n")
+        logging.info("Done")
 
 
 class RetrivalLM():
-    def __init__(self, model_name: str, data_store: DataStore, **kwargs):
+    def __init__(self, data_store: DataStore,
+                 search_type: str = 'similarity',
+                 search_kwargs: dict = {'k': 3},
+                 task='text-generation',
+                 model_name='mistralai/Mistral-7B-Instruct-v0.2',
+                 max_new_token_length=100):
         """
         Args:
-            model_name (str): Model name for pipeline(). Should be available for AutoModelForCausalLM.
             data_store (DataStore): DataStore storing chunked documents.
-        **kwargs: for as_retriever()
+            search_type (str, optional): Type of search that the Retriever should perform.
+                Defaults to 'similarity';
+                Options: 'similarity', 'similarity_score_threshold', 'mmr'.
+            search_kwargs (dict, optional): Keyword arguments to pass to the search function.
+                Defaults to {'k': 5} fo 'similarity';
+                Example {'score_threshold': 0.5} for 'similarity_score_threshold';
+                Example {'fetch_k': 20, 'lambda_mult': 0.5} for 'mmr'.
+            task (str, optional): Transformer pipeline task.
+                Defaults to 'text-generation'.
+                Options: 'text-geneartion', 'text2text-generation'.
+            model_name (str, optional): Model name for pipeline(). Should be consistent with the pipeline task.
+            max_new_token_length (int, optional): Maximum number of tokens to generate. Defaults to 100.
         """
-        # Retriver config
-        if kwargs:
-            self.retriever = data_store.vector_store.as_retriever(**kwargs)
-        else:
-            self.retriever = data_store.vector_store.as_retriever(
-                search_type="similarity", search_kwargs={"k": 5})
+        # Retriever config
+        self.retriever = data_store.vector_store.as_retriever(search_type=search_type,
+                                                              search_kwargs=search_kwargs)
 
         # LLM config
         torch.cuda.empty_cache()
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         if not tokenizer.pad_token:
             tokenizer.pad_token = tokenizer.eos_token
-        self.llm = pipeline(task="text-generation",
-                            model=model_name,
-                            tokenizer=tokenizer,
-                            max_new_tokens=512,
-                            torch_dtype=torch.bfloat16,
-                            device=DEVICE)
+        self.task = task
+        self.llm = pipeline(task=task, model=model_name, tokenizer=tokenizer,
+                            max_new_tokens=max_new_token_length,
+                            torch_dtype=torch.bfloat16, device=DEVICE)
 
-        # Prompt config
-        self.prompt_template = self.llm.tokenizer.apply_chat_template(
-            PROMPT_IN_CHAT_FORMAT, tokenize=False, add_generation_prompt=True
-        )
-
-    def query_answer(self, query: Query, **kwargs):
+    def qa(self, query: Query, **kwargs):
         """
         Retrive "context", generate "answer", and update them in the Query dictionary.
 
@@ -247,27 +217,68 @@ class RetrivalLM():
         **kwargs: for calling pipeline()
         """
         query['context'] = self.retriever.invoke(query['question'])
-        context = ['\n'+doc.page_content for doc in query['context']]
-        prompt = self.prompt_template.format(context=context, question=query['question'])
-        response = self.llm(prompt, return_full_text=False, **kwargs)
-        query['answer'] = response[0]["generated_text"].strip()  # type: ignore
+        context = '\n----------\n'.join([f'Context {i+1}:\n{doc.page_content}'
+                                         for i, doc in enumerate(query['context'])])
+        prompt = PROMPT_IN_CHAT_FORMAT.format(context=context, question=query['question'])
+        if self.task == 'text-generation':
+            kwargs['return_full_text'] = False
+        response = self.llm(prompt, **kwargs)
+        query['answer'] = response[0]["generated_text"]  # type: ignore
         torch.cuda.empty_cache()
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    # Required parameters
+    parser.add_argument('data_dir', type=str, help='Raw data directory')
+    # Retriver parameters
+    parser.add_argument('--embedding_model', type=str, default='all-mpnet-base-v2')
+    parser.add_argument('--chunk_size', type=int, default=500)
+    parser.add_argument('--chunk_overlap', type=int, default=100)
+    parser.add_argument('--is_semantic_chunking', type=bool, default=False)
+    parser.add_argument('--search_type', type=str, default='similarity')
+    # LLM parameters
+    parser.add_argument('--llm_model', type=str, default='mistralai/Mistral-7B-Instruct-v0.2')
+    parser.add_argument('--task', type=str, default='text-generation')
+    parser.add_argument('--max_new_token_length', type=int, default=100)
+    # Logging paramters
+    parser.add_argument('--log_file_mode', type=str, default='a')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
 
-    set_logger('rag_pipeline')
+    args = parse_args()
+    set_logger('rag_pipeline', file_mode=args.log_file_mode)
+
+    logging.info(f'Configuration:\n{vars(args)}')
     logging.info(f'Device: {DEVICE}')
 
-    args = parse_args()
+    # Experiment hyperparameters
+    search_config = {'k': 3}
+    generation_config = GenerationConfig(
+        max_new_tokens=100,
+        do_sample=True,
+        temperature=0.01,
+        top_p=0.95,
+        repetition_penalty=1.2,
+    )
 
-    data_store = DataStore(model_name=args.embedding_model, chunker_name=args.chunker_name,
+    # Set up models
+    data_store = DataStore(model_name=args.embedding_model, data_dir=args.data_dir,
                            chunk_size=args.chunk_size, chunk_overlap=args.chunk_overlap,
-                           is_upsert_data=False)
-    rag_model = RetrivalLM(model_name=args.llm_model, data_store=data_store)
+                           is_semantic_chunking=args.is_semantic_chunking)
+    rag_model = RetrivalLM(data_store=data_store,
+                           search_type=args.search_type,
+                           search_kwargs=search_config,
+                           task=args.task, model_name=args.llm_model)
 
-    question = "When is the Vintage Pittsburgh retro fair taking place?"
-    query = Query(question=question, context=[], answer="")
-    rag_model.query_answer(query)
-
-    logging.info(f"Question: {question}\nAnswer: {query['answer']}")
+    # Run RAG
+    question = [
+        "What type of artworks can one explore at The Andy Warhol Museum in Pittsburgh?",
+        "When is the Vintage Pittsburgh retro fair taking place?"
+    ]
+    for question in question:
+        query = Query(question=question, context=[], answer="")
+        rag_model.qa(query, **generation_config.to_dict())
+        print(f"\n{question}\n{query['answer']}\n")
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import logging
 from typing import List
 
+import numpy as np
 from langchain.text_splitter import TextSplitter
+from langchain_core.documents import Document
 
 
 def set_logger(log_name: str, file_mode='a'):
@@ -19,6 +21,23 @@ def set_logger(log_name: str, file_mode='a'):
 
     logging.basicConfig(level=logging.INFO,
                         handlers=[file_handler, console_handler])
+
+
+def get_chunk_max_length(chunks: List[Document]) -> int:
+    """
+    Find the upper bound of the length of the chunks to filter too long chunks.
+
+    Args:
+        chunks (List[Document]): A list semantic chunks.
+
+    Returns:
+        int: Upper bound of the non-outliers as the max length to filter too long chunks.
+    """
+    lengths = np.array([len(chunk.page_content) for chunk in chunks])
+    q1 = np.percentile(lengths, 25)
+    q3 = np.percentile(lengths, 75)
+    upper_bound = q3 + 1.5 * (q3 - q1)
+    return (int(upper_bound // 100) + 1) * 100
 
 
 class ParagraphTextSplitter(TextSplitter):

9d0b57631ee7f3c7a653b87f0e87a734c6bcf4fc
xx-aws <xiaoxu1094@gmail.com>
2025-03-16 04:28:54 +0000

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -8,6 +8,7 @@ from typing import List, TypedDict
 import torch
 from dotenv import load_dotenv
 from huggingface_hub import login
+from langchain import hub
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_core.documents import Document
 from langchain_experimental.text_splitter import SemanticChunker
@@ -37,15 +38,6 @@ pc = Pinecone()
 DEVICE = ('cuda' if torch.cuda.is_available() else
           'mps' if torch.backends.mps.is_available() else 'cpu')
 
-# Set prompt template
-PROMPT_IN_CHAT_FORMAT = """\
-{context}
-----------
-Using the information contained in the context,
-give a concise, accurate answer to the question.
-Question: {question}
-"""
-
 
 class Query(TypedDict):
     """
@@ -61,9 +53,9 @@ class Query(TypedDict):
 
 class DataStore():
     def __init__(self, model_name: str, data_dir: str,
-                 chunk_size=500, chunk_overlap=100,
+                 chunk_size=1000, chunk_overlap=100,
                  filename_pattern='**/*.txt',
-                 is_semantic_chunking=False, is_upsert_data=False):
+                 is_semantic_chunking=True, is_new_index=True):
         """
         Args:
             model_name (str): Name of HuggingFaceEmbeddings model.
@@ -72,70 +64,56 @@ class DataStore():
             chunk_overlap (int, optional): Defaults to 100.
             filename_pattern (str, optional): "glob" parameter for DirectoryLoader. Defaults to '**/*.txt'.
             is_semantic_chunking (bool, optional):
-                True for using SemanticChunker, otherwise RecursiveCharacterTextSplitter. Defaults to False.
-            is_upsert_data (bool, optional):
-                Whether to upsert documents to vector store. Defaults to False.
+                True for mainly using SemanticChunker and False for RecursiveCharacterTextSplitter.
+                Defaults to True.
+            is_new_index (bool, optional):
+                Whether to load, chunk, and upsert documents to vector store.
+                Defaults to True.
         """
         # Data config
         self.data_dir = data_dir
         self.filename_pattern = filename_pattern
 
         # Chunking config
-        chunker_name = 'semantic' if is_semantic_chunking else 'recursive'
         self.is_semantic_chunking = is_semantic_chunking
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
 
         # Index config
-        index_name = f'{model_name}-{chunker_name}-{chunk_size}-{chunk_overlap}'.lower()
         self.index_name = re.sub(r'[^a-zA-Z0-9]', '-',
-                                 index_name)  # Rename for Pinecone index name requirement
-        self.is_upsert_data = is_upsert_data
+                                 f'{model_name}-{chunk_size}-{chunk_overlap}'.lower())
 
         # Embedding model config
         self.embeddings = HuggingFaceEmbeddings(model_name=model_name,
                                                 model_kwargs={'device': DEVICE})
         self.dimension = len(self.embeddings.embed_documents(['test'])[0])
 
-        # Initialize Pinecone Index and Vector Store
-        self.pc_index = self.get_pinecone_index()
-        self.vector_store = PineconeVectorStore(index=self.pc_index, embedding=self.embeddings)
-        self.retriever = self.vector_store.as_retriever()
+        # Initialize Vector Store
+        if is_new_index:  # Create new index and vector store and upsert documents
+            logging.info(f"Create Pinecone index: {self.index_name}")
+            self.vector_store = self.get_vector_store()
+            chunks = self.chunk_documents()
+            logging.info(f'Upserting {len(chunks)} chunks to Index "{self.index_name}"...')
+            self.vector_store.add_documents(chunks)
+        else:  # Load existing vector store
+            logging.info(f"Use existing Pinecone index: {self.index_name}")
+            pc_index = pc.Index(self.index_name)
+            self.vector_store = PineconeVectorStore(index=pc_index, embedding=self.embeddings)
 
-        if self.is_upsert_data:
-            self.upsert_vector_store()
-
-    def get_pinecone_index(self) -> Index:
+    def get_vector_store(self) -> PineconeVectorStore:
         """
-        Set up Pinecone Index with mathing dimension and similarity score.
-
-        Returns:
-            Index
+            PineconeVectorStore
         """
-        if self.index_name not in pc.list_indexes().names():
-            logging.info(f"Create new Pinecone Index")
-            self.create_new_index()
-        else:
-            index_dict = pc.describe_index(self.index_name)
-            if index_dict['dimension'] != self.dimension:
-                logging.info(f"Recreate Pinecone Index due to mismatch in model dimension")
-                pc.delete_index(self.index_name)
-                self.create_new_index()
+        if self.index_name in pc.list_indexes().names():
+            pc.delete_index(self.index_name)
+        pc.create_index(name=self.index_name, dimension=self.dimension,
+                        spec=ServerlessSpec(cloud="aws", region="us-east-1"))
         pc_index = pc.Index(self.index_name)
-        return pc_index
+        vector_store = PineconeVectorStore(index=pc_index, embedding=self.embeddings)
+        return vector_store
 
-    def create_new_index(self):
-        pc.create_index(
-            name=self.index_name,
-            dimension=self.dimension,
-            spec=ServerlessSpec(cloud="aws", region="us-east-1")
-        )
-        self.is_upsert_data = True  # Upsert data as long as creating new index
-
-    def upsert_vector_store(self):
+    def chunk_documents(self) -> List[Document]:
         """
-        Update and insert chunked documents to vector store.
-
         Returns:
             list[Document]
         """
@@ -146,35 +124,33 @@ class DataStore():
         docs = loader.load()
 
         # Chunk documents
-        if self.is_semantic_chunking:  # SemanticChunker + potentail RecursiveCharacterTextSplitter
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size,
+                                                       chunk_overlap=self.chunk_overlap)
+        if self.is_semantic_chunking:  # SemanticChunker + RecursiveCharacterTextSplitter
             semantic_splitter = SemanticChunker(self.embeddings)
             semantic_chunks = [chunk for doc in tqdm(docs, desc='Chunk docs')
                                for chunk in semantic_splitter.split_documents([doc])]
-            # Sub-chunk long documents where the document length falls in the upper outlier range
-            max_length = min(get_chunk_max_length(semantic_chunks), 40000)
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size=max_length,
-                                                           chunk_overlap=int(max_length*0.1))
             chunks = []
             for chunk in semantic_chunks:
-                if len(chunk.page_content) < max_length:
+                if len(chunk.page_content) <= self.chunk_size:
                     chunks.append(chunk)
-                else:  # Document length outliers
+                else:  # Sub-chunk documents with length larger than chunk_size
                     chunks.extend(text_splitter.split_documents([chunk]))
         else:  # Pure RecursiveCharacterTextSplitter
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size,
-                                                           chunk_overlap=self.chunk_overlap)
             chunks = text_splitter.split_documents(docs)
 
         # Add IDs for the documents
+        result = []
+        doc_text = set()
         for i, doc in tqdm(enumerate(chunks), total=len(chunks), desc='Add doc id'):
+            if doc.page_content in doc_text:  # Deduplicate
+                continue
             id_text = f"{i}_{doc.metadata['source']}"
             id_text = re.sub(r'[^\w]', '_', id_text).lower()
             doc.id = id_text
-
-        # Index documents
-        logging.info(f'Upserting {len(chunks)} chunks to Index "{self.index_name}"...')
-        self.vector_store.add_documents(chunks)
-        logging.info("Done")
+            doc_text.add(doc.page_content)
+            result.append(doc)
+        return result
 
 
 class RetrivalLM():
@@ -182,8 +158,7 @@ class RetrivalLM():
                  search_type: str = 'similarity',
                  search_kwargs: dict = {'k': 3},
                  task='text-generation',
-                 model_name='mistralai/Mistral-7B-Instruct-v0.2',
-                 max_new_token_length=100):
+                 model_name='mistralai/Mistral-7B-Instruct-v0.2'):
         """
         Args:
             data_store (DataStore): DataStore storing chunked documents.
@@ -198,21 +173,27 @@ class RetrivalLM():
                 Defaults to 'text-generation'.
                 Options: 'text-geneartion', 'text2text-generation'.
             model_name (str, optional): Model name for pipeline(). Should be consistent with the pipeline task.
-            max_new_token_length (int, optional): Maximum number of tokens to generate. Defaults to 100.
         """
         # Retriever config
-        self.retriever = data_store.vector_store.as_retriever(search_type=search_type,
-                                                              search_kwargs=search_kwargs)
+        self.retriever = data_store.vector_store.as_retriever(
+            search_type=search_type,
+            search_kwargs=search_kwargs
+        )
 
         # LLM config
         torch.cuda.empty_cache()
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        if not tokenizer.pad_token:
-            tokenizer.pad_token = tokenizer.eos_token
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, model_max_length=4096)
+        if not self.tokenizer.pad_token:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
         self.task = task
-        self.llm = pipeline(task=task, model=model_name, tokenizer=tokenizer,
-                            max_new_tokens=max_new_token_length,
-                            torch_dtype=torch.bfloat16, device=DEVICE)
+        self.llm = pipeline(
+            task=task,
+            model=model_name,
+            tokenizer=self.tokenizer,
+            torch_dtype=torch.bfloat16,
+            device=DEVICE
+        )
+        self.prompt_template = hub.pull("langchain-ai/retrieval-qa-chat")
 
     def qa(self, query: Query, **kwargs):
         """
@@ -224,9 +205,8 @@ class RetrivalLM():
         """
         retrieved_docs = self.retriever.invoke(query['question'])
         query['context'] = [doc.page_content for doc in retrieved_docs]
-        context = '\n----------\n'.join([f'Context {i+1}:\n{doc}'
-                                         for i, doc in enumerate(query['context'])])
-        prompt = PROMPT_IN_CHAT_FORMAT.format(context=context, question=query['question'])
+        prompt = self.prompt_template.format(context='\n'.join(query['context']),
+                                             input=query['question'])
         if self.task == 'text-generation':
             kwargs['return_full_text'] = False
         response = self.llm(prompt, **kwargs)
@@ -237,16 +217,16 @@ class RetrivalLM():
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     # Required parameters
-    parser.add_argument('data_dir', type=str, help='Raw data directory')
+    parser.add_argument('--data_dir', type=str, default='raw_data',
+                        help='Raw data directory')
     # Retriver parameters
     parser.add_argument('--embedding_model', type=str, default='all-mpnet-base-v2')
-    parser.add_argument('--chunk_size', type=int, default=500)
+    parser.add_argument('--chunk_size', type=int, default=1000)
     parser.add_argument('--chunk_overlap', type=int, default=100)
-    parser.add_argument('--is_semantic_chunking', type=bool, default=False)
-    parser.add_argument('--search_type', type=str, default='similarity')
+    parser.add_argument('--is_semantic_chunking', type=bool, default=True)
     # LLM parameters
-    parser.add_argument('--llm_model', type=str, default='mistralai/Mistral-7B-Instruct-v0.2')
-    parser.add_argument('--task', type=str, default='text-generation')
+    parser.add_argument('--llm_model', type=str, default='google/flan-t5-large')
+    parser.add_argument('--task', type=str, default='text2text-generation')
     parser.add_argument('--max_new_token_length', type=int, default=100)
     # Logging paramters
     parser.add_argument('--log_file_mode', type=str, default='a')
@@ -261,33 +241,47 @@ if __name__ == '__main__':
     logging.info(f'Configuration:\n{vars(args)}')
     logging.info(f'Device: {DEVICE}')
 
-    search_config = {'k': 3}
-    generation_config = GenerationConfig(
-        max_new_tokens=100,
-        do_sample=True,
-        temperature=0.01,
-        top_p=0.95,
-        repetition_penalty=1.2,
+    # Set up DataStore
+    data_store = DataStore(
+        model_name=args.embedding_model,
+        data_dir=args.data_dir,
+        chunk_size=args.chunk_size,
+        chunk_overlap=args.chunk_overlap,
+        is_semantic_chunking=args.is_semantic_chunking,
+        is_new_index=False,
     )
 
-    # Set up models
-    data_store = DataStore(model_name=args.embedding_model, data_dir=args.data_dir,
-                           chunk_size=args.chunk_size, chunk_overlap=args.chunk_overlap,
-                           is_semantic_chunking=args.is_semantic_chunking)
-    rag_model = RetrivalLM(data_store=data_store,
-                           search_type=args.search_type,
-                           search_kwargs=search_config,
-                           task=args.task, model_name=args.llm_model)
+    # Set up Retriver
+    search_type = 'similarity'
+    search_config = {'k': 3}
+    rag_model = RetrivalLM(
+        data_store=data_store,
+        search_type=search_type,
+        search_kwargs=search_config,
+        task=args.task,
+        model_name=args.llm_model
+    )
+
+    # Set up generator
+    generation_config = {
+        'max_new_tokens': 50,
+        # 'do_sample': True,
+        # 'temperature': 0.01,
+        # 'top_p': 0.95,
+        # 'repetition_penalty': 1.2,
+        'use_cache': True,
+    }
 
     # Run RAG
     question = [
-        "What type of artworks can one explore at The Andy Warhol Museum in Pittsburgh?",
-        "When is the Vintage Pittsburgh retro fair taking place?"
+        "What kind of food is featured in the Lenten Fish Fry?",
+        "What food is Picklesburgh centered around?",
+        "What year was the Pittsburgh Penguins team founded?"
     ]
     queries = []
-    for question in question:
+    for question in tqdm(question):
         query = Query(question=question, context=[], answer="")
-        rag_model.qa(query, **generation_config.to_dict())
+        rag_model.qa(query, **generation_config)
         queries.append(query)
 
     with open('output.json', 'w') as f:
